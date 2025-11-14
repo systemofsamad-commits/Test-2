8b4d78fc69ac4e09443061206d56c59bca7e9c80
systemofsamad-commits <systemofsamad@gmail.com>
2025-11-14 13:57:54 +0500

    Test-05

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,6 @@
+# utils/__init__.py
+# ✅ ИСПРАВЛЕНО: Удален импорт Database, который создавал циклический импорт
+
 from .validators import validate_name, validate_phone, format_phone
 from helpers import is_admin, extract_id, get_grade_from_progress, get_student_by_id
 
